95a317baa8329cdff002dbdd70fb2bd0cf07a38f
Giovanni Asproni <gasproni@asprotunity.com>
2014-02-03 18:12:02 +0000

    small fixes

--- example/start_exchange_server.sh
+++ example/start_exchange_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -classpath tools/joda-time-2.2/joda-time-2.2.jar:tools/hsqldb_2_3_0/lib/hsqldb.jar:tools/joker/joker-0.1.0-uberjar.jar:target/libs/example.jar com.asprotunity.exchange.server.main.Main $@
+java -classpath tools/joda-time-2.2/joda-time-2.2.jar:tools/hsqldb_2_3_0/lib/hsqldb.jar:tools/joker/joker-0.1.0-uberjar.jar:build/libs/example.jar com.asprotunity.exchange.server.main.Main $@
